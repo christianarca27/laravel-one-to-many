21556035ea3669ea3bc5498b94f907cc92ee6101
christianarca27 <christian.arca.mail@gmail.com>
2023-05-23 14:33:55 +0200

    feat: creato model Type + creazione della tabella nel db tramite migration e riempimento tramite seeder + aggiunta della chiave esterna type_id alla tabella Projects

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin\Type;
+use Faker\Generator;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Generator $generator)
+    {
+        $types = ['Front-End', 'Back-end', 'Full-Stack'];
+
+        for ($i = 0; $i < count($types); $i++) {
+            $newType = new Type();
+
+            $newType->name = $types[$i];
+            $newType->slug = Str::slug($newType->name);
+            $newType->description = $generator->text(50);
+
+            $newType->save();
+        }
+    }
+}
